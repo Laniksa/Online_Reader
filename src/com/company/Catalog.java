874d62f612cb59a6af3ececda6319c776dedb648
Laniksa <rmashkovtseva@gmail.com>
2023-01-07 23:19:33 +0300

    ошибка с созданием книги, выход из программы, кодировка при выводе массива.

--- src/com/company/Catalog.java
+++ src/com/company/Catalog.java
@@ -5,44 +5,62 @@ import java.util.Scanner;
 
 public class Catalog {
     public static int numberBook = 0;
+    public static Book[] library = new Book[10] ;
     public static void menu() {
         Scanner scan = new Scanner(System.in);
         int num;
-        System.out.println("Введите действие: \n" +
-                "1. Добавить книгу \n" +
-                "2. Показать все книги \n" +
-                "3. Выход");
+        while (true) {
+            System.out.println("Введите действие: \n" +
+                    "1. Добавить книгу \n" +
+                    "2. Показать все книги \n" +
+                    "3. Выход");
 
-        try {
-            num = scan.nextInt();// проверка на ввод
-            if (num < 1 || num > 3) {
-                throw new InputMismatchException();
+
+            try {
+                num = scan.nextInt();// проверка на ввод
+                switch (num){
+                    case 1:
+                        addBook(createBook(scan));
+                        break;
+                    case 2:
+                        System.out.println(library);//проблема с кодировкой
+                        break;
+                    case 3:
+                        System.out.println("Программа завершена");// не выходит из программы
+                        break;
+                }
+                if (num < 1 || num > 3) {
+                    throw new InputMismatchException();
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Введите корректное число из меню");
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Введите корректное число из меню");
-        }
 
+
+        }
     }
-    public static Book inputBook(Scanner sc){
-        System.out.println("Введите название книги :");
+    public static Book createBook(Scanner sc){
         String tempName = sc.nextLine();
-        System.out.println("Введите автора книги :");
+        System.out.println("Введите название книги :");
         String tempAuthor = sc.nextLine();
-        System.out.println("Введите дом издания книги :");
+        System.out.println("Введите автора книги :");
         String tempPublishingHouse = sc.nextLine();
-        System.out.println("Введите год издания книги :");
+        System.out.println("Введите дом издания книги :");// после ввода зацикливается
         int tempPublished = sc.nextInt();
-        System.out.println("Введите количество страниц книги :");
+        System.out.println("Введите год издания книги :");
         int tempPages = sc.nextInt();
+        System.out.println("Введите количество страниц книги :");
+
         return new Book(tempName,tempAuthor,tempPublishingHouse,tempPublished,tempPages);
     }
     public static void addBook(Book book){
-        Book[] library = new Book[10] ;
+        //Book[] library = new Book[10] ;
         library[numberBook] = book;
         numberBook++;
 
     }
     public static void main(String[] args) {
+        menu();
 
     }
 
