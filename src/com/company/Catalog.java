d659bfe252c3ea0b482b267876dda4d1f0a65f9f
Регина <rmashkovtseva@gmail.com>
2023-01-09 15:52:49 +0300

    Исправлены ошибки Вопрос о корректном зацикливании меню с которрым можно выйти из меню. Вопрос как пристроить проверку при вводе пункта меню.

--- src/com/company/Catalog.java
+++ src/com/company/Catalog.java
@@ -5,72 +5,80 @@ import java.util.Scanner;
 
 public class Catalog {
     public static int numberBook = 0;
-    public static Book[] library = new Book[10] ;
-    public static void menu() {
+    public static int maxBook = 15;
+    public static Book[] library = new Book[maxBook] ;
+
+    public static boolean menu() {
         Scanner scan = new Scanner(System.in);
         int num;
-        while (true) {
-            System.out.println("Введите действие: \n" +
+        System.out.println("Введите действие: \n" +
                     "1. Добавить книгу \n" +
                     "2. Показать все книги \n" +
                     "3. Выход");
 
 
-            try {
-                num = scan.nextInt();// проверка на ввод
-
-                if (num < 1 || num > 3) {
-                    throw new InputMismatchException();
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Введите корректное число из меню");
-            }
-            switch (num){//не читается
+//        try {
+//            num = scan.nextInt();// проверка на ввод
+//
+//            if (num < 1 || num > 3) {
+//                throw new InputMismatchException();
+//            }
+//
+//        } catch (InputMismatchException e) {
+//            System.out.println("Введите корректное число из меню");
+//        }
+        num = scan.nextInt();//пришлось дублировать сос строкой 21,иначе не видит
+            switch (num) {//не читается
                 case 1:
                     addBook(createBook(scan));
                     break;
                 case 2:
-                    for(int i=0; i<10; i++){
+                    for (int i = 0; i < maxBook; i++) {
                         System.out.println(library[i]);
                     }
 
                     break;
                 case 3:
 
-                    System.out.println("Программа завершена");// не выходит из программы
+                    System.out.println("\n Программа завершена");// не выходит из программы
                     return false;
 
 
+
+        }
+            return true;
+    }
+        public static Book createBook (Scanner scan){
+
+            scan.nextLine();
+
+            System.out.println("Введите название книги :");
+            String tempName = scan.nextLine();
+
+            System.out.println("Введите автора книги :");
+            String tempAuthor = scan.nextLine();
+
+            System.out.println("Введите дом издания книги :");// после ввода зацикливается
+            String tempPublishingHouse = scan.nextLine();
+
+            System.out.println("Введите год издания книги :");
+            int tempPublished = scan.nextInt();
+
+            System.out.println("Введите количество страниц книги :");
+            int tempPages = scan.nextInt();
+
+            return new Book(tempName, tempAuthor, tempPublishingHouse, tempPublished, tempPages);
+        }
+        public static void addBook(Book book){
+            library[numberBook] = book;
+            numberBook++;
+
+        }
+        public static void main (String[]args){
+        while (true) {// не могу придумать ка зациклить меню без while
+            menu();
+        }
+
         }
     }
-    public static Book createBook(Scanner sc){
 
-        System.out.println("Введите название книги :");
-        String tempName = sc.nextLine();
-
-        System.out.println("Введите автора книги :");
-        String tempAuthor = sc.nextLine();
-
-        System.out.println("Введите дом издания книги :");// после ввода зацикливается
-         String tempPublishingHouse = sc.nextLine();
-
-        System.out.println("Введите год издания книги :");
-        int tempPublished = sc.nextInt();
-
-        System.out.println("Введите количество страниц книги :");
-        int tempPages = sc.nextInt();
-
-        return new Book(tempName,tempAuthor,tempPublishingHouse,tempPublished,tempPages);
-    }
-    public static void addBook(Book book){
-        //Book[] library = new Book[10] ;
-        library[numberBook] = book;
-        numberBook++;
-
-    }
-    public static void main(String[] args) {
-        menu();
-
-    }
-
-}
